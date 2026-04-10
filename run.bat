9f6438bf8f36f945573bec139c79c0bd1387d4ba
dhanuvagman006 <dhnaushinsit@gmail.com>
2026-04-10 20:40:56 +0530

    new changes

--- run.bat
+++ run.bat
@@ -3,10 +3,12 @@ echo Starting Solar Energy Prediction Platform...
 
 echo Starting Django Backend Server...
 cd solar_backend
+pip install -r requirements.txt
 start cmd /k "python manage.py runserver 0.0.0.0:8000"
 
 echo Starting React Vite Frontend...
 cd ../solar_frontend
+npm i
 start cmd /k "npm run dev"
 
 echo Both servers are starting up! Check the new terminal windows.
